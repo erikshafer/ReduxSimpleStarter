5cd0475d630619cbe8e9e6eb759446a34fe89a4d
Erik Shafer <erik.shafer@gmail.com>
2018-01-29 15:47:46 -0600

    Component Structure unit.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,18 @@ import ReactDOM from 'react-dom';
 // Create a new component. This component should produce some HTML.
 // When creating a Component, we are creating a Class of a Component. Not an instance.
 // We need to instantiate before we put it in the DOM.
-const App = function () {
+// const App = function () {
+//     return <div>Hi!</div>;
+// }
+// ES6: More or less, using a fat arrow is identical-ish to using the function keyword.
+const App = () => {
     return <div>Hi!</div>;
 }
 
+
 // Take this component's generated HTML and put it on the page (in the DOM).
 // Wrapping something with tags will instantiate it. From `App` to <App />`, etc.
-ReactDOM.render(<App />);
+// Second argument is stating where it will be inserted in the DOM
+//  e.g.    document.querySelector('.container')
+
+ReactDOM.render(<App />, document.querySelector('.container'));
