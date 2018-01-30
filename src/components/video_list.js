da6b4f85c63dcad59b5b6f1c0c3509d3eb794467
Erik Shafer <erik.shafer@gmail.com>
2018-01-30 14:24:50 -0600

    List item keys

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-// In a functional component, props is an argument.
-// In a class component, props are available anywhere as `this.props`.
-// Be aware of this when refactoring! Update references from `props` to `this.props`.
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
