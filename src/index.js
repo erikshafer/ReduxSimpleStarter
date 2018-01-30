1984eff37cfc6a670c840c94ff49cb85806e1113
Erik Shafer <erik.shafer@gmail.com>
2018-01-30 13:34:20 -0600

    refactoring functional components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyC4z84LUAjS63TYpMiSfOWOeeYVZwYN8kY';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-// Create a new component. This component should produce some HTML.
-// When creating a Component, we are creating a Class of a Component. Not an instance.
-// We need to instantiate before we put it in the DOM.
 const App = () => {
   return (
     <div>
@@ -19,11 +15,5 @@ const App = () => {
     </div>
   );
 }
-
-
-// Take this component's generated HTML and put it on the page (in the DOM).
-// Wrapping something with tags will instantiate it. From `App` to <App />`, etc.
-// Second argument is stating where it will be inserted in the DOM
-//  e.g.    document.querySelector('.container')
 
 ReactDOM.render(<App />, document.querySelector('.container'));
